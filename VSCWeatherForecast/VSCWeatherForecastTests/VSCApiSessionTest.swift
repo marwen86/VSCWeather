1cdf8647cf6580aac87c19a8abf5071079f66053
marouane youssef <marouane_youssef@gcarrefour.com>
2017-09-26 08:22:10 +0200

    fix code syntax

--- VSCWeatherForecast/VSCWeatherForecastTests/VSCApiSessionTest.swift
+++ VSCWeatherForecast/VSCWeatherForecastTests/VSCApiSessionTest.swift
@@ -11,22 +11,22 @@ import Alamofire
 
 class VSCApiSessionTest: XCTestCase {
     
-    func test_weatherList_thatURLCorrect() {
+    func testWeatherList_thatURLCorrect() {
         let requstUrl = String(format: API_DOWNLAD_WEATHER_URL, arguments: [API_BASE_URL, "paris", "16", API_KEY])
         XCTAssertEqual(requstUrl, "http://api.openweathermap.org/data/2.5/forecast/daily?q=paris&mode=Json&units=metric&cnt=16&lang=fr&appid=b1938b15024d33334163ef66c06849aa")
     }
     
-    func test_currentWeather_thatURLCorrect() {
+    func testCurrentWeather_thatURLCorrect() {
        let requstUrl = String(format: API_API_DOWNALOD_CURRENT_WEATHER, arguments: [API_BASE_URL, "paris", API_KEY])
         XCTAssertEqual(requstUrl, "http://api.openweathermap.org/data/2.5/weather?q=paris&mode=Json&units=metric&lang=fr&appid=b1938b15024d33334163ef66c06849aa")
     }
     
-    func test_WeatherIcon_thatURLCorrect() {
+    func testWeatherIcon_thatURLCorrect() {
        let requstUrl = String(format: API_DOWNLOAD_ICON_URL, arguments: [API_BASE_URL, "03n"])
         XCTAssertEqual(requstUrl, "http://api.openweathermap.org/img/w/03n")
     }
     
-    func test_CheckThatTheNetworkResponseIsEqualToTheExpectedResult() {
+    func testValidResponseNetworkWithValidurl() {
         
         let expectations = expectation(description: "The Response result match the expected results")
         let requstUrlStr = String(format: API_DOWNLAD_WEATHER_URL, arguments: [API_BASE_URL, "paris", "1", API_KEY])
